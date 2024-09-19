acafcdda583246af247c070973da4e7b00e04553
beln <belen.teruel@mi.unc.edu.ar>
2024-09-19 12:48:31 -0300

    Modularizado, mock de testing

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,11 +1,17 @@
 import './App.css';
 import Login from './components/Login/Login.jsx';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';  // para manejar rutas
+import Home from './components/Home/Home.jsx';
 
 function App () {
-    return (
-        <div>  
-            <Login />
-        </div>
+    return (    
+        <Router>  
+            <Routes>
+                <Route path="/" element={<Navigate to="/login"/>} />
+                <Route path="/login" element={<Login/>} />
+                <Route path="/home" element={<Home/>} />
+            </Routes>
+        </Router>
     );
 }
 
--- src/containers/App/components/Login/Login.jsx
+++ src/containers/App/components/Login/Login.jsx
@@ -1,74 +1,71 @@
 import Button from "../../../../components/Button/Button.jsx";
 import Form from "../../../../components/Form/Form.jsx";
 import { useEffect, useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
-import './Login.css';
+import { useNavigate } from 'react-router-dom'; 
+import fetchMock from 'fetch-mock';
 
+import '../../../../utils/Constants.js'
+import './Login.css';
+import { LoginHelpText, LOGIN_URL } from "../../../../utils/Constants.js";
+
+
+// fetchMock.post(LOGIN_URL, { Id: 3 });   // MOCK TEST 
 
 function Login () {
-    const helpText = "Debe tener entre 1 y 10 caracteres.";
-    
-    const [username, setUsername] = useState('');
-    const [token, setToken] = useState('');
-    
+    const navigate = useNavigate(); 
+    const [username, setUsername] = useState('');  
+
     /**
-     *  UseEffect is a hook that executes the first time the component is rendered
+     *  Hook that executes when the component is rendered
      *  It is used to get the username from the local storage if it exists
     */
     useEffect(() => {
-        const savedUserName = localStorage.getItem('username');
-        const savedToken = localStorage.getItem('token');
-        if (savedUserName && savedToken) {
+       const savedUserName = localStorage.getItem('username');
+        if (savedUserName) {
             setUsername(savedUserName);
-            setToken(savedToken);
         }
     }, []);    
     
-    const handleChange = (e) => {
-        setUsername(e.target.value);
-    };
+    const handleChange = (e) => { setUsername(e.target.value); };
+    const checkInput = (input) => { return input.length > 0 && input.length <= 10; }
     
-    const checkInput = (input) => {
-        return input.length > 0 && input.length <= 10;
+    async function register () {
+        try {            
+            const response = await fetch(LOGIN_URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username })  
+            });
+            
+            if (!response.ok) {
+                throw new Error("Error al enviar solicitud 'crear usuario'. ");
+            }
+
+            const data = await response.json();
+            const { Id } = data;
+
+            alert(`Usuario ${username} creado exitosamente con Id: ${Id}`);
+            localStorage.setItem('username', username); 
+            localStorage.setItem('id_usser', Id);
+            navigate('/home');
+            
+        } catch (error) {
+            alert("Error al crear usuario. " + error.message);
+        }
     }
     
-    const handleClick = async () => {
-        
+    const handleInput = () => {
         if (checkInput(username)) {
-            const port = 8000;
-            const url = `http://localhost:${port}/login`;
-            setToken(uuidv4());
-
-            try {
-                await fetch(url, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ username, token })  
-                });
-                localStorage.setItem('username', username);  
-                localStorage.setItem('token', token);
-                alert(`Usuario ${username} creado exitosamente`);
+            const savedUserName = localStorage.getItem('username');
+            if (username !== savedUserName) {                    
+                register();
+            } else {
+                navigate('/home');
             }
-            catch (error) {
-                alert("Error al crear usuario. " + error.message);
-                setToken('');  // Comentar esta linea para testear lo de abajo
-                
-                /**
-                 * Esto es solo para testear que se guarden los datos en el local storage
-                 * (comentar ademas linea previa "setToken('')")
-                 * 
-                 * localStorage.setItem('username', username);
-                 * localStorage.setItem('token', token);
-                 * const saveduser = localStorage.getItem('username');
-                 * const savedtoken = localStorage.getItem('token');
-                 * alert("Usuario guardado exitosamente " + saveduser + " " + savedtoken);
-                 */
-            }
-        }
-        else {
-            alert("Nombre " + helpText);
+        } else {
+            alert("Nombre " + LoginHelpText);
         }
     };
 
@@ -80,12 +77,12 @@ function Login () {
                     label="Registro de Usuario"
                     type="text"
                     placeholder="Ingresar nombre de usuario"
-                    helpText={helpText}
+                    helpText={LoginHelpText}
                     id="inputUsername"
                     onChange={handleChange}
                     value={username}
                 /> 
-                <Button label="Ingresar" onClick={handleClick}/>
+                <Button label="Ingresar" onClick={handleInput}/>
             </div>
         </div>
     );
