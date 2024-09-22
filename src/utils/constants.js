9a5883434ddc5cdfed7ac1c914826d595521f76c
Alvaro <alvaro.medina@mi.unc.edu.ar>
2024-09-22 00:24:03 -0300

    Agrego constantes

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+// constants.js
+
+export const PORT = 8000;
+export const BASE_URL = `http://localhost:${PORT}/home/get-lobbies`;
+
+/** URLs */
+// export const LOGIN_URL = `${BASE_URL}/login`;
+// export const SIGNUP_URL = `${BASE_URL}/signup`;
+// export const GAMES_URL = `${BASE_URL}/games`;
+// export const PROFILE_URL = `${BASE_URL}/profile`;
+
+/** Otras constantes */
+export const MAX_PLAYERS = 4;
+export const MIN_PLAYERS = 1;
