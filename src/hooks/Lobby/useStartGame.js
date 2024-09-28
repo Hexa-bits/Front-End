5be0132346e22a9c95fb93634dc844ced308c51f
beln <belen.teruel@mi.unc.edu.ar>
2024-09-28 17:21:04 -0300

    .

--- src/hooks/Lobby/useStartGame.js
+++ src/hooks/Lobby/useStartGame.js
@@ -1,27 +1,27 @@
-// import { GAME_START_URL, GAME } from '../../utils/Constants.js';
+import { GAME_START_URL, GAME } from '../../utils/Constants.js';
 // import fetchMock from 'fetch-mock';
 // fetchMock.put(GAME_START_URL, 200);   // MOCK TEST 
 
 
-// export const startGame = async (gameId, navigate) => {
-//     const game_id = parseInt(gameId, 10);
-//     try {
-//         const response = await fetch(GAME_START_URL, {
-//             method: 'PUT',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify({ game_id: game_id }),
-//         });
-//         if (!response.ok) {
-//             const errorMessage = await response.text();
-//             throw new Error(errorMessage || response.statusText);
-//         }
-//         console.log(`Juego ${game_id} iniciado exitosamente`);
-//         navigate(GAME);
-//     }
-//     catch (error) {
-//         console.log("No se pudo iniciar el juego. " + error.message);
-//     }
-// };
+export const startGame = async (gameId, navigate) => {
+    const game_id = parseInt(gameId, 10);
+    try {
+        const response = await fetch(GAME_START_URL, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ game_id: game_id }),
+        });
+        if (!response.ok) {
+            const errorMessage = await response.text();
+            throw new Error(errorMessage || response.statusText);
+        }
+        console.log(`Juego ${game_id} iniciado exitosamente`);
+        navigate(GAME);
+    }
+    catch (error) {
+        console.log("No se pudo iniciar el juego. " + error.message);
+    }
+};
 
