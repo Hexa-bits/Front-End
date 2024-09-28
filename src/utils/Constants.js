b020d8690370b01720890c369601cd6f43ec0b2c
beln <belen.teruel@mi.unc.edu.ar>
2024-09-28 20:18:13 -0300

    GAME path

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -5,6 +5,7 @@ export const LOGIN = '/login';
 export const HOME = '/home';
 export const GAME = '/game';
 export const LOBBY = '/home/lobby';
+export const SETGAME = '/home/create-config';
 
 /** URLS */
 export const CONFIG_URL = `http://localhost:${PORT}/home/create-config`;
@@ -12,6 +13,7 @@ export const LOGIN_URL = `http://localhost:${PORT}${LOGIN}`;
 export const LOBBY_URL = `http://localhost:${PORT}${LOBBY}`;
 export const HOME_URL = `http://localhost:${PORT}${HOME}/get-lobbies`;
 export const GAME_LEAVE_URL = `http://localhost:${PORT}/game/leave`;
+export const GAME_START_URL = `http://localhost:${PORT}/game/start-game`;
 
 /** MENSAJES */
 export const LoginHelpText = "Debe tener entre 1 y 10 caracteres.";
