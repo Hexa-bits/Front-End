8225de8536b647b81ee72d773c4a4e4eb54f2ddc
beln <belen.teruel@mi.unc.edu.ar>
2024-09-28 20:13:43 -0300

    svg cartas movs + mostrar

---
+++ src/components/Game/MovCards/MovCards.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './MovCards.css';
+
+function MovCards() {
+    return ( 
+        <div className="mov-cards-container">
+            <div className='mov-card'>
+                <img src="../../../../assets/Movements/mov1.svg" alt="mov1" />
+                <img src="../../../../assets/Movements/mov2.svg" alt="mov2" />
+                <img src="../../../../assets/Movements/mov3.svg" alt="mov3" />
+            </div>
+        </div>
+    );
+}
+
+export default MovCards;
