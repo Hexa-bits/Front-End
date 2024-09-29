91133f146d82d2c96da14c8e9fcdccd591065232
jazmin <jazmin.fernandez@mi.unc.edu.ar>
2024-09-29 18:14:31 -0300

    Abandonar in game

--- src/containers/App/components/Game/Game.jsx
+++ src/containers/App/components/Game/Game.jsx
@@ -1,11 +1,17 @@
-import { useEffect } from "react";
+import React from "react";
+//import "./game.css";
+import { useNavigate } from "react-router-dom";
+import { leaveGame } from "../../../../hooks/Lobby/useLeaveGame.js";
+import Button from "../../../../components/Button/Button.jsx";
 
 function Game() {
-    return ( 
-        <div>
-            <h1>Game</h1>
-        </div>
-    );
+  const gameId = localStorage.getItem("game_id");
+  const navigate = useNavigate();
+  const handleClick = () => {
+    leaveGame(gameId, navigate);
+  };
+
+  return <Button label="Dejar Partida en juego" onClick={handleClick} />;
 }
 
 export default Game;
