f35e01d65ddc13967b422917a17e8bcc0c6c3f3a
beln <belen.teruel@mi.unc.edu.ar>
2024-09-30 20:10:20 -0300

    pequenos cambios

--- src/hooks/Home/useGames.js
+++ src/hooks/Home/useGames.js
@@ -15,15 +15,15 @@ function useGames(initialGames = []) {
                 }
                 const data = await response.json();
                 setGames(data);
-                setError(null); // Resetea el error si la petición es exitosa
+                setError(null); 
             } catch (error) {
                 console.error("Error fetching lobbies:", error);
-                setError(error.message); // Guarda el mensaje de error en el estado
+                setError(error.message); 
             }
         };
 
         fetchGames();
-    }, []); // Esto se ejecuta solo una vez, cuando el componente se monta
+    }, []); 
 
     useEffect(() => {
         const ws = new WebSocket(HOME_URL_WS);
@@ -35,21 +35,22 @@ function useGames(initialGames = []) {
         ws.onmessage = (event) => {
             console.log('Mensaje recibido:', event.data);
             try {
-                // Reemplaza las comillas simples por comillas dobles
-                const formattedString = event.data.replace(/'/g, '"');
+                // Verifica si el mensaje es un JSON válido
+                const gamesData = JSON.parse(event.data);
 
-                // Intenta parsear el string JSON
-                const gamesData = JSON.parse(formattedString);
-
-                console.log('Games:', gamesData.length); // Muestra la longitud del array de juegos
-                setGames(gamesData); // Actualiza el estado de juegos
+                // Verifica si los datos recibidos son diferentes a los actuales
+                const isDifferent = JSON.stringify(games) !== JSON.stringify(gamesData);
+                if (isDifferent) {
+                    console.log('Actualizando juegos:', gamesData.length);
+                    setGames(gamesData); 
+                } else {
+                    console.log('Datos recibidos son iguales a los actuales, no se actualiza el estado');
+                }
             } catch (error) {
                 console.error('Error parsing JSON:', error);
                 setError('Error al procesar los datos del juego');
             }
         };
-
-
 
         ws.onclose = (event) => {
             console.log('Disconnected from WebSocket server', event);
@@ -64,42 +65,9 @@ function useGames(initialGames = []) {
                 ws.close();
             }, 1000);
         };
-    }, []);
+    }, [games]);
 
     return { games, error };
 }
 
 export default useGames;
-// 
-
-// import { useState } from 'react';
-// import { HOME_URL } from '../../utils/Constants.js';
-// import usePooling from './usePooling.js'
-
-// function useGames(initialGames = []) {
-//     const [games, setGames] = useState(initialGames);
-//     const [error, setError] = useState(null); // Cambia el estado inicial a null
-
-//     const fetchGames = async () => {
-//         try {
-//             const response = await fetch(`${HOME_URL}`);
-//             if (!response.ok) {
-//                 throw new Error('Error en la respuesta del servidor');
-//             }
-//             const data = await response.json();
-
-//             setGames(data);
-//             setError(null); // Resetea el error si la petición es exitosa
-//         } catch (error) {
-//             console.error("Error fetching lobbies:", error);
-//             setError(error.message); // Guarda el mensaje de error en el estado
-//         }
-//     };
-
-//     usePooling(fetchGames, 500);
-
-//     return { games, error }; // Retorna el error
-// }
-
-// export default useGames;
-
