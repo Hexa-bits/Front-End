09f0c30bbda103be17a8735aafca9fc803677667
beln <belen.teruel@mi.unc.edu.ar>
2024-10-06 22:45:09 -0300

    ws correcciones

--- src/containers/App/components/Lobby/Lobby.jsx
+++ src/containers/App/components/Lobby/Lobby.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import './Lobby.css';
 
 import LobbyCard from "../../../../components/Lobby/Card/LobbyCard.jsx"
@@ -8,20 +8,29 @@ import LobbyButtons from "../../../../components/Lobby/Buttons/LobbyButtons.jsx"
 import { useLobby } from "../../../../hooks/Lobby/useLobby.js";
 import { LeaveGame } from "../../../../hooks/Lobby/leaveGame.jsx";
 import { StartGame } from "../../../../hooks/Lobby/startGame.jsx";
-import { useNavigate } from 'react-router-dom';
+import { HOME, GAME } from "../../../../utils/Constants.js";
 
 function Lobby() {
     const location = useLocation();
     const navigate = useNavigate();
     const {isOwner, gameId} = location.state || {};
-    const {players, gameName, maxPlayers} = useLobby(gameId, navigate);
+
+    const {players, gameName, maxPlayers, activeGame, cancelGame} = useLobby(gameId, navigate);
+
+    useEffect(() => {
+        if (cancelGame) { navigate(HOME); }
+        if (activeGame) { navigate(GAME); }
+    }, [cancelGame, activeGame]);
 
     return (
         <div className="lobby-overlay">
             <div className='lobby-container'>
                 <LobbyCard gameName={gameName} maxPlayers={maxPlayers} />
                 <LobbyList players={players} />
-                <LobbyButtons isOwner={isOwner} onLeaveGame={LeaveGame(navigate)} onStartGame={StartGame(navigate)}/>
+                <LobbyButtons 
+                    isOwner={isOwner} 
+                    onLeaveGame={LeaveGame(navigate)} 
+                    onStartGame={StartGame(navigate)}/>
             </div>
         </div>
     );
--- src/hooks/Lobby/useLobby.js
+++ src/hooks/Lobby/useLobby.js
@@ -1,36 +1,33 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, act } from 'react';
 import { LOBBY_URL, WS_LOBBY_URL, HOME, GAME } from '../../utils/Constants.js';
 
-export const useLobby = ((gameId, navigate) => {
+export const useLobby = (gameId) => {
     const [players, setPlayers] = useState([]);
     const [gameName, setGameName] = useState('');
     const [maxPlayers, setMaxPlayers] = useState(0);
-    const [activeGame , setActiveGame] = useState(false); 
+    const [activeGame, setActiveGame] = useState(false);
     const [cancelGame, setCancelGame] = useState(false);
-    const [isWsConnected, setIsWsConnected] = useState(false);
-
 
     // Seteo info del lobby al montar el componente
     useEffect(() => {
         const getGameInfo = async () => {
             try {
-                const response = await fetch( LOBBY_URL + gameId, {
+                const response = await fetch(`${LOBBY_URL}${gameId}`, {
                     method: "GET",
                 });
                 if (!response.ok) {
-                    throw new Error("Error al obtener información del juego.");
+                    throw new Error("http: Error al obtener información del juego.");
                 }
                 const data = await response.json();
-                console.log("Obteniendo información del juego...");
+                console.log("http: Obteniendo información del juego...");
     
                 setPlayers(data.name_players || []);
                 setGameName(data.game_name);
                 setMaxPlayers(data.max_players);
                 setActiveGame(data.start_owner);
                 setCancelGame(data.cancel_owner);
-                setIsWsConnected(true);
             } catch (error) {
-                console.log("Error al obtener información del juego. " + error.message);
+                console.log("http: Error al obtener información del juego. " + error.message);
             }
         }
         getGameInfo();
@@ -38,34 +35,37 @@ export const useLobby = ((gameId, navigate) => {
 
     // Mantener actualizado el lobby con WebSocket
     useEffect(() => {
-        if (!isWsConnected) return;
-
-        const ws = new WebSocket(WS_LOBBY_URL + gameId);
+        const ws = new WebSocket(`${WS_LOBBY_URL}${gameId}`);
+        
         ws.onmessage = (event) => {
             const message = event.data;
-            if (message){console.log('Actualización de Lobby.')}
-            if (message.startsWith('{') || message.startsWith('[')) {
-                try {
-                    const lobbyData = JSON.parse(message);
-                    setPlayers(lobbyData.name_players || []);
-                    setActiveGame(lobbyData.start_owner);
-                    setCancelGame(lobbyData.cancel_owner);
-                } catch (error) {
-                    console.error('Error parsing JSON:', error);
+            if (message) {
+                console.log('Actualización de Lobby.');
+                if (message.startsWith('{') || message.startsWith('[')) {
+                    try {
+                        const lobbyData = JSON.parse(message);
+                        console.log('Lobby actualizado:', lobbyData);
+                        setPlayers(lobbyData.name_players || []);
+                        setActiveGame(lobbyData.start_owner);
+                        setCancelGame(lobbyData.cancel_owner);
+
+                    } catch (error) {
+                        console.error('Error parsing JSON:', error);
+                    }
                 }
             }
-            if (cancelGame){ navigate(HOME); }
-            if (activeGame){ navigate(GAME); }
         };
+
         ws.onerror = (error) => {
             console.error('WebSocket error:', error);
         };
-        return () => { ws.close(); }
 
-    }, [isWsConnected]);
+        return () => {
+            if (ws.readyState === WebSocket.OPEN) { 
+                ws.close();
+            }
+        };
+    }, [gameId]);
 
-
-    return {players, gameName, maxPlayers, activeGame, cancelGame};
-});
-
-
+    return { players, gameName, maxPlayers, activeGame, cancelGame };
+};
