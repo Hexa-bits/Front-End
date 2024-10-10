61f8cba12b4ebec1ca7d5e098a2267b316432b69
alvaro-mediina <alvaro.medina@mi.unc.edu.ar>
2024-10-10 16:57:52 -0300

    Mock de cartas para corroborar el formato correcto

--- src/components/Game/Board/Board.jsx
+++ src/components/Game/Board/Board.jsx
@@ -2,38 +2,65 @@ import './Board.css';
 import BoxCard from './BoxCard/BoxCard';
 import useSelectedCards from '../../../hooks/Game/Board/useSelectedCards';
 
+//A modo de Ejemplo
+const cardData = [
+    { x: 0, y: 0, color: 'red' },
+    { x: 0, y: 1, color: 'red' },
+    { x: 0, y: 2, color: 'red' },
+    { x: 0, y: 3, color: 'red' },
+    { x: 0, y: 4, color: 'red' },
+    { x: 0, y: 5, color: 'red' },
+    { x: 1, y: 0, color: 'blue' },
+    { x: 1, y: 1, color: 'blue' },
+    { x: 1, y: 2, color: 'blue' },
+    { x: 1, y: 3, color: 'blue' },
+    { x: 1, y: 4, color: 'blue' },
+    { x: 1, y: 5, color: 'blue' },
+    { x: 2, y: 0, color: 'green' },
+    { x: 2, y: 1, color: 'green' },
+    { x: 2, y: 2, color: 'green' },
+    { x: 2, y: 3, color: 'green' },
+    { x: 2, y: 4, color: 'green' },
+    { x: 2, y: 5, color: 'green' },
+    { x: 3, y: 0, color: 'yellow' },
+    { x: 3, y: 1, color: 'yellow' },
+    { x: 3, y: 2, color: 'yellow' },
+    { x: 3, y: 3, color: 'yellow' },
+    { x: 3, y: 4, color: 'yellow' },
+    { x: 3, y: 5, color: 'yellow' },
+    { x: 4, y: 0, color: 'red' },
+    { x: 4, y: 1, color: 'red' },
+    { x: 4, y: 2, color: 'red' },
+    { x: 4, y: 3, color: 'red' },
+    { x: 4, y: 4, color: 'red' },
+    { x: 4, y: 5, color: 'red' },
+    { x: 5, y: 0, color: 'blue' },
+    { x: 5, y: 1, color: 'blue' },
+    { x: 5, y: 2, color: 'blue' },
+    { x: 5, y: 3, color: 'blue' },
+    { x: 5, y: 4, color: 'blue' },
+    { x: 5, y: 5, color: 'blue' },
+
+];
 function Board ({ isTurn }){
 
-    //A modo de Ejemplo
-    const boxCards = [
-        ["red", "yellow", "green", "blue", "red", "yellow"],
-        ["green", "blue", "red", "yellow", "green", "blue"],
-        ["yellow", "green", "blue", "red", "yellow", "green"],
-        ["blue", "red", "yellow", "green", "blue", "red"],
-        ["red", "yellow", "green", "blue", "red", "yellow"],
-        ["green", "blue", "red", "yellow", "green", "blue"],
-    ];
 
     const { selectedCards, handlerSelectedCard } = useSelectedCards(isTurn);
 
     return (
         <div className="Board">
             <div className="BoxCards">
-                {boxCards.map((row, rowIndex) => (
-                    <div key={rowIndex} className="BoardRow">
-                        {row.map((color, colIndex) => {
-                            const index = `${rowIndex}-${colIndex}`;
-                            return (
-                                <BoxCard
-                                    key={colIndex}
-                                    color={color}
-                                    isSelected={selectedCards.includes(index)}
-                                    onClick={() => handlerSelectedCard(rowIndex, colIndex)}
-                                />
-                            );
-                        })}
-                    </div>
-                ))}
+                {cardData.map(({ x, y, color }) => {
+                        const index = `${x}-${y}`;
+                        return (
+                            <BoxCard
+                                key={index}
+                                color={color}
+                                isSelected={selectedCards.includes(index)}
+                                onClick={() => handlerSelectedCard(x, y)}
+                            />
+                        );
+                    })}
             </div>
         </div>
     );
