2cc0a968b60ea9cec1b883ab15a708a597c11a29
alvaro-mediina <alvaro.medina@mi.unc.edu.ar>
2024-10-10 16:40:03 -0300

    Estilos para el tablero mejorado

--- src/hooks/Game/Board/useSelectedCards.js
+++ src/hooks/Game/Board/useSelectedCards.js
@@ -4,18 +4,19 @@ function useSelectedCards(isTurn) {
     const [selectedCards, setSelectedCards] = useState([]);
 
     const handlerSelectedCard = (rowIndex, colIndex) => {
-        if(!isTurn) return;
+        if (!isTurn) return; // Si no es el turno, no hace nada
 
-        const index = `${rowIndex}-${colIndex}`;
+        const index = `${rowIndex}-${colIndex}`; // Índice de la carta seleccionada
 
         if (selectedCards.includes(index)) {
+            // Si la carta ya está seleccionada, la deselecciona
             setSelectedCards(selectedCards.filter(card => card !== index));
         } else {
+            // Si ya hay dos fichas seleccionadas, deselecciona las anteriores
             if (selectedCards.length >= 2) {
-                // Si ya hay dos fichas seleccionadas, resetea la selección y selecciona la nueva
-                setSelectedCards([index]);
+                setSelectedCards([]);
             } else {
-                // Si hay menos de dos fichas, agrega la nueva
+                // Si hay menos de dos fichas seleccionadas, añade la nueva
                 setSelectedCards([...selectedCards, index]);
             }
         }
