a02ecf7b94afbb3fe493f6d6bc34f90d59414501
jazzffernandez <jazmin.fernandez@mi.unc.edu.ar>
2024-10-11 01:15:03 -0300

    WS terminado + tests

---
+++ src/tests/Game/board.test.js
@@ -0,0 +1,67 @@
+import { renderHook, waitFor } from "@testing-library/react";
+import { afterEach, beforeEach, expect, vi } from "vitest";
+import renewBoard from "../../hooks/Game/Board/renewBoard.js";
+import { GAME_BOARD_URL } from "../../utils/Constants.js";
+
+const gameId = 1;
+describe("renewBoard", () => {
+  let mockFetchBoxCards;
+
+  beforeEach(() => {
+    mockFetchBoxCards = vi.fn(() =>
+      Promise.resolve({
+        ok: true,
+        json: () =>
+          Promise.resolve([
+            { x: 1, y: 1, color: 1 },
+            { x: 2, y: 2, color: 2 },
+          ]),
+      })
+    );
+    global.fetch = mockFetchBoxCards;
+
+    vi.spyOn(console, "log").mockImplementation(() => {});
+    vi.spyOn(console, "error").mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("inicializa los valores", () => {
+    const { result } = renderHook(() => renewBoard(gameId));
+    expect(result.current.boxCards).toEqual([]);
+  });
+
+  it("hace el pedido al boxcards", async () => {
+    const { result } = renderHook(() => renewBoard(gameId));
+
+    await waitFor(() => {
+      expect(mockFetchBoxCards).toHaveBeenCalledWith(GAME_BOARD_URL + gameId, {
+        method: "GET",
+      });
+    });
+
+    waitFor(() => {
+      expect(result.current.boxCards).toEqual([
+        { x: 1, y: 1, color: 1 },
+        { x: 2, y: 2, color: 2 },
+      ]);
+    });
+  });
+
+  it("maneja errores", async () => {
+    mockFetchBoxCards.mockImplementationOnce(() =>
+      Promise.reject(new Error("Fetch error"))
+    );
+    const { result } = renderHook(() => renewBoard());
+
+    await waitFor(() => {
+      expect(result.current.boxCards).toEqual([]); // Se espera que boxCards siga vacío
+      expect(console.error).toHaveBeenCalledWith(
+        "Error al obtener las fichas:",
+        expect.any(Error)
+      );
+    });
+  });
+});
