1ffa2218c751c7c8de8cd63e8e34563e5a7c4a96
jazzffernandez <jazmin.fernandez@mi.unc.edu.ar>
2024-10-13 20:31:48 -0300

    Owner abandona y todos se van a home

--- src/components/Lobby/Buttons/LobbyButtons.jsx
+++ src/components/Lobby/Buttons/LobbyButtons.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import Button from "../../Button/Button.jsx";
-import './LobbyButtons.css';
+import "./LobbyButtons.css";
 
-function LobbyButtons({ isOwner, onLeaveGame, onStartGame, oneJoined}) {
-    return (
-        <div className="btn-container">
-            {isOwner ? (
-                <>
-                    <Button label="Abandonar" onClick={onLeaveGame} className="btn-leave" disabled={true}/>
-                    <Button label="Iniciar" onClick={onStartGame} className="btn-start" disabled={oneJoined}/>
-                </>
-            ) : (
-                <Button label="Abandonar" onClick={onLeaveGame} className="btn-leave"/>
-            )}
-        </div>
-    );
-};
+function LobbyButtons({ isOwner, onLeaveGame, onStartGame, oneJoined }) {
+  return (
+    <div className="btn-container">
+      {isOwner ? (
+        <>
+          <Button
+            label="Abandonar"
+            onClick={onLeaveGame}
+            className="btn-leave"
+            disabled={false}
+          />
+          <Button
+            label="Iniciar"
+            onClick={onStartGame}
+            className="btn-start"
+            disabled={oneJoined}
+          />
+        </>
+      ) : (
+        <Button label="Abandonar" onClick={onLeaveGame} className="btn-leave" />
+      )}
+    </div>
+  );
+}
 
 export default LobbyButtons;
