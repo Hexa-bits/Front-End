5a0334010a0398227b25802378481c986527eccf
jazzffernandez <jazmin.fernandez@mi.unc.edu.ar>
2024-10-15 13:34:56 -0300

    El owner puede cancelar la partida abandonando + corri los botones

--- src/components/Lobby/Buttons/LobbyButtons.jsx
+++ src/components/Lobby/Buttons/LobbyButtons.jsx
@@ -2,19 +2,29 @@ import React from "react";
 import Button from "../../Button/Button.jsx";
 import "./LobbyButtons.css";
 
-function LobbyButtons({ isOwner, onLeaveGame, onStartGame, oneJoined}) {
-    return (
-        <div className="btn-container">
-            {isOwner ? (
-                <>
-                    <Button label="ABANDONAR" onClick={onLeaveGame} className="btn-leave" disabled={true}/>
-                    <Button label="INICIAR" onClick={onStartGame} className="btn-start" disabled={oneJoined}/>
-                </>
-            ) : (
-                <Button label="ABANDONAR" onClick={onLeaveGame} className="btn-leave"/>
-            )}
-        </div>
-    );
-};
+function LobbyButtons({ isOwner, onLeaveGame, onStartGame, oneJoined }) {
+  return (
+    <div className="btn-container">
+      {isOwner ? (
+        <>
+          <Button
+            label="ABANDONAR"
+            onClick={onLeaveGame}
+            className="btn-leave"
+            disabled={false}
+          />
+          <Button
+            label="INICIAR"
+            onClick={onStartGame}
+            className="btn-start"
+            disabled={oneJoined}
+          />
+        </>
+      ) : (
+        <Button label="ABANDONAR" onClick={onLeaveGame} className="btn-leave" />
+      )}
+    </div>
+  );
+}
 
 export default LobbyButtons;
