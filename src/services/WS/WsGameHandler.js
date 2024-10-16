b6b688a33f08894320b7f90dcd39c57dc1e74a72
jazzffernandez <jazmin.fernandez@mi.unc.edu.ar>
2024-10-15 22:44:31 -0300

    modificación de mjes de ws. Falta revisar

--- src/services/WS/WsGameHandler.js
+++ src/services/WS/WsGameHandler.js
@@ -13,18 +13,30 @@ const wsGameHandler = (
 
     ws.onmessage = (event) => {
       const message = event.data;
-
-      if (message === "Terminó turno") {
-        console.log("Mensaje de turno recibido");
-        fetchTurnData();
-        fetchFigs();
+      // if(message==="Se unió jugador en lobby" || message === "Abandonó jugador en lobby"){
+      // }
+      if (message === "Inició la partida") {
+        console.log("Mensaje de partida iniciada");
+        fetchFigs(); //cartas fig y mov
         fetchMovs();
-        fetchBoxCards();
+        fetchTurnData(); //info del turno
+        fetchBoxCards(); //ver tablero
+      } else if (message === "Terminó turno") {
+        console.log("Mensaje de turno recibido");
+        fetchFigs(); //cartas fig y mov
+        fetchMovs();
+        fetchTurnData(); //info del turno
+        //fetchBoxCards();
+      } else if (message === "Hay modificación en el tablero") {
+        console.log("Mensaje recibido modificación tablero");
+        fetchBoxCards(); //ver tablero
       } else if (message === "Hay Ganador") {
         console.log("Mensaje de ganador recibido");
         getWinner();
-      } else if (message === "Hay modificación de Tablero") {
-        fetchBoxCards();
+      } else if (message === "Actualizar cartas de movimiento") {
+        fetchMovs(); //cartas fig y mov
+      } else if (message === "Actualizar cartas de figura") {
+        fetchFigs(); //cartas fig y mov
       }
     };
   }, [ws, fetchTurnData, getWinner, fetchFigs, fetchMovs, fetchBoxCards]);
