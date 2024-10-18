242141ee8127cad1e72daddc79fddb7295d54a48
BelenTeruel <belen.teruel@mi.unc.edu.ar>
2024-10-18 12:07:21 -0300

    vista de cartas de movs restantes

--- src/components/Game/OtherPlayers/OtherPlayers.jsx
+++ src/components/Game/OtherPlayers/OtherPlayers.jsx
@@ -1,18 +1,24 @@
 import FigCards from "../FigCards/FigCards";
 import PlayerName from "../PlayerName/PlayerName";
+import BackMovs from "../MovCards/BackMovs";
 import "./OtherPlayers.css";
 function OtherPlayers({players}) {
+
+
     return ( 
         <>
         { players.map((player, index) => (
-            <div className="each-player">
-              
-                <div key={index}></div>
+            <div className="each-player" key={index}>
                 <div className="player-name"> 
                     <PlayerName label={index} player={player.nombre} />   
                  </div>
                 <div className="player-figs"> 
                     <FigCards fig_cards={player.fig_cards} />
+                </div>
+                <div className="cant-mov-container">
+                    {Array.from({ length: player.mov_cant }, () => (
+                        <img key={index} src="/assets/Movements/back-mov.svg" alt={index} className="back-mov-image" />
+                    ))}
                 </div>
             </div>
         ))}
