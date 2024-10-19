25f39e083fc04cf34fbcf3bdde518cc38e1c14f6
BelenTeruel <belen.teruel@mi.unc.edu.ar>
2024-10-19 18:08:36 -0300

    agrego tests

--- src/tests/Game/getOthersInfo.test.js
+++ src/tests/Game/getOthersInfo.test.js
@@ -5,6 +5,8 @@ import { afterEach, beforeEach, expect, vi } from "vitest";
 const playerId = 1;
 const gameId = 2;
 
+const url = `game_id=${gameId}&player_id=${playerId}`;
+
 describe ("getOthersInfo", () => {
     let mockPlayers;
 
@@ -37,6 +39,7 @@ describe ("getOthersInfo", () => {
 
         global.fetch = mockPlayers;
         vi.spyOn(console, "log").mockImplementation(() => {});
+        vi.spyOn(console, "error").mockImplementation(() => {});
     });
 
     afterEach(() => {
@@ -47,5 +50,55 @@ describe ("getOthersInfo", () => {
         const { result } = renderHook(() => getOthersInfo(gameId, playerId));
         expect(result.current.infoPlayers).toEqual([]);
     })
+
+    it("Fetches cards correctly", async () => {
+        const { result } = renderHook(() => getOthersInfo(gameId, playerId));
+        const { fetchInfoPlayers } = result.current;
+        await fetchInfoPlayers();
+        
+        waitFor(() => {
+            expect(mockPlayers).toHaveBeenCalledWith(url, { method: "GET", });
+            expect(result.current.infoPlayers).toEqual([
+                {
+                    nombre: "Player1", 
+                    fig_cards: [
+                        {id: 1, fig: 4},
+                        {id: 2, fig: 5},
+                        {id: 3, fig: 6}
+                    ], 
+                    mov_cant: 1
+                },
+                {
+                    nombre: "Player2", 
+                    fig_cards: [
+                        {id: 4, fig: 7},
+                        {id: 5, fig: 8},
+                        {id: 6, fig: 9}
+                    ], 
+                    mov_cant: 3
+                }
+            ]);
+        });
+    });
+
+    it ("Handles fetch error gracefully", async () => {
+        mockPlayers = vi.fn(() => 
+            Promise.resolve({
+                ok: false,
+                json: () => Promise.resolve({ error: "Error al obtener la información de los otros jugadores." }),
+            })
+        );
+
+        global.fetch = mockPlayers;
+
+        const { result } = renderHook(() => getOthersInfo(gameId, playerId));
+        const { fetchInfoPlayers } = result.current;
+        await fetchInfoPlayers();
+
+        waitFor(() => {
+            expect(mockPlayers).toHaveBeenCalledWith(url, { method: "GET", });
+            expect().toHaveBeenCalledWith("Error al obtener la información de los otros jugadores.");
+        });
+    });
 
 });
