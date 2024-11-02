a8b2558db3b143e6013cf0bcbf6d13931e3da8b5
BelenTeruel <belen.teruel@mi.unc.edu.ar>
2024-11-02 18:33:49 -0300

    mock response

--- src/services/Game/Cards/getOthersInfo.js
+++ src/services/Game/Cards/getOthersInfo.js
@@ -4,17 +4,39 @@ import { GET_PLAYERS_INFO } from "../../../utils/Constants";
 
 function getOthersInfo (gameId, playerId) {
     
+    const mock = [
+
+        {
+            nombre: "Player1", 
+            fig_cards: [
+                {id: 1, fig: 4, blocked: true},
+                {id: 2, fig: 5, blocked: false},
+                {id: 3, fig: 6, blocked: false},
+            ], 
+            mov_cant: 1
+        },
+        {
+            nombre: "Player2", 
+            fig_cards: [
+                {id: 4, fig: 7, blocked: false},
+                {id: 5, fig: 8, blocked: false},
+                {id: 6, fig: 9, blocked: false},
+            ], 
+            mov_cant: 3
+        }
+    ];
+
     const [infoPlayers, setInfoPlayers] = useState([]);
     const url = `${GET_PLAYERS_INFO}game_id=${gameId}&player_id=${playerId}`;
     
     const fetchInfoPlayers = useCallback (async () => {
         try{
-            const response = await fetch(url, { method: "GET" });
-            if (!response.ok) {
-                console.log("Error en la petición");
-            }
-            const data = await response.json();
-            setInfoPlayers(data);
+            // const response = await fetch(url, { method: "GET" });
+            // if (!response.ok) {
+            //     console.log("Error en la petición");
+            // }
+            // const data = await response.json();
+            setInfoPlayers(mock);
 
         } catch (error) {
             console.log(error);
