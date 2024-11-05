51f17a99f91ba7e186221f726ea983fb7c948cd3
jazzffernandez <jazmin.fernandez@mi.unc.edu.ar>
2024-11-05 10:52:47 -0300

    intento refresh (no funciona aun)

--- src/services/WS/WsGameService.js
+++ src/services/WS/WsGameService.js
@@ -1,41 +1,93 @@
-let ws = null ;
+// let ws = null;
+// export const createWsGameInstance = (url) => {
+//   if (!ws) {
+//     ws = new WebSocket(url);
+
+//     ws.onopen = () => {
+//       console.log("Game WebSocket abierto.");
+//     };
+
+//     ws.onclose = () => {
+//       console.log("Game WebSocket cerrado.");
+//     };
+
+//     ws.onmessage = (event) => {
+//       console.log("Game WS Mensaje recibido:", event.data);
+//       setMessages((prevMessages) => [...prevMessages, event.data]);
+//     };
+
+//     ws.onerror = (error) => {
+//       console.error("Game WebSocket error:", error);
+//     };
+//   }
+//   return ws;
+// };
+
+// export const closeWsGameInstance = () => {
+//   if (ws && ws.readyState === WebSocket.OPEN) {
+//     ws.close();
+//     ws = null;
+//   }
+// };
+
+// export const sendMessage = (message) => {
+//   if (ws && ws.readyState === WebSocket.OPEN) {
+//     ws.send(message);
+//   } else {
+//     console.error(
+//       "No se puede enviar el mensaje, Game WebSocket no está abierto."
+//     );
+//   }
+// };
+
+// export const getWsGameInstance = () => ws;
+
+let ws = null;
 export const createWsGameInstance = (url) => {
-    if (!ws) {
-        ws = new WebSocket(url);
-        
-        ws.onopen = () => {
-            console.log("Game WebSocket abierto.");
-        };
+  const savedUrl = localStorage.getItem("wsUrl");
+  if (savedUrl) {
+    url = savedUrl;
+  }
 
-        ws.onclose = () => {
-            console.log("Game WebSocket cerrado.");
-        };
+  if (!ws) {
+    ws = new WebSocket(url);
+    localStorage.setItem("wsUrl", url);
+    ws.onopen = () => {
+      console.log("Game WebSocket abierto.");
+    };
 
-        ws.onmessage = (event) => {
-            console.log("Game WS Mensaje recibido:", event.data);
-            setMessages(prevMessages => [...prevMessages, event.data]);
-        };
+    ws.onclose = () => {
+      console.log("Game WebSocket cerrado.");
+    };
 
-        ws.onerror = (error) => {
-            console.error('Game WebSocket error:', error);
-        };
-    }
-    return ws;
-}
+    ws.onmessage = (event) => {
+      console.log("Game WS Mensaje recibido:", event.data);
+      setMessages((prevMessages) => [...prevMessages, event.data]);
+    };
+
+    ws.onerror = (error) => {
+      console.error("Game WebSocket error:", error);
+    };
+  }
+  return ws;
+};
 
 export const closeWsGameInstance = () => {
-    if (ws && ws.readyState === WebSocket.OPEN) {
-        ws.close();
-        ws = null;
-    }
-}
+  if (ws && ws.readyState === WebSocket.OPEN) {
+    ws.close();
+    localStorage.removeItem("wsUrl");
+    ws = null;
+  }
+};
 
 export const sendMessage = (message) => {
-    if (ws && ws.readyState === WebSocket.OPEN) {
-        ws.send(message);
-    } else {
-        console.error("No se puede enviar el mensaje, Game WebSocket no está abierto.");
-    }
-}
+  if (ws && ws.readyState === WebSocket.OPEN) {
+    ws.send(message);
+  } else {
+    console.error(
+      "No se puede enviar el mensaje, Game WebSocket no está abierto."
+    );
+  }
+};
 
 export const getWsGameInstance = () => ws;
