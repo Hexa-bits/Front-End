0fc489f5dc9b8bb060bfb75cd98404e780709bca
alvaro-mediina <alvaro.medina@mi.unc.edu.ar>
2024-11-04 23:36:22 -0300

    Efecto para enviar

--- src/components/Game/Chat/Chat.jsx
+++ src/components/Game/Chat/Chat.jsx
@@ -1,31 +1,42 @@
 import "./Chat.css";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import WSMessages from "../../../services/WS/Chat/WSMessages";
 import Button from "../../Button/Button";
 
-    function Chat({ ws, playerId }) {
+function Chat({ ws, playerId }) {
     const [input, setInput] = useState("");
     const [messages, setMessages] = useState([]);
+    
+    // Crear una referencia para el final de la lista de mensajes
+    const messagesEndRef = useRef(null);
 
-    // Llama a WSMessages y maneja la recepción de mensajes
-    const { sendMessage } = WSMessages({ ws, onMessageReceived: (data) => {
-        setMessages(prevMessages => [...prevMessages, data]);
-    }});
+    const { sendMessage } = WSMessages({
+        ws,
+        onMessageReceived: (data) => {
+            setMessages((prevMessages) => [...prevMessages, data]);
+        }
+    });
 
     const handleSendMessage = (msg) => {
         if (msg.trim()) {
             sendMessage(playerId, msg);
-            setInput(""); 
+            setInput("");
         }
     };
 
     const handleKeyDown = (event) => {
         if (event.key === 'Enter') {
-            event.preventDefault(); 
+            event.preventDefault();
             handleSendMessage(input);
         }
     };
 
+    // Usar un efecto para hacer scroll hasta el final cuando se reciba un nuevo mensaje
+    useEffect(() => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [messages]);
 
     return (
         <div className="chat__container">
@@ -34,6 +45,8 @@ import Button from "../../Button/Button";
                     {messages.map((msg, index) => (
                         <li key={index}><strong>{msg.player_name}</strong> <br/> {msg.msg}</li>
                     ))}
+                    {/* Este elemento vacío actúa como el punto de referencia para el scroll */}
+                    <div ref={messagesEndRef} />
                 </ul>
             </div>
             <div className="chat__entries">
