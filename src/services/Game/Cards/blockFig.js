22e335d90c326ee602b9ddf7d09c7865da81048b
BelenTeruel <belen.teruel@mi.unc.edu.ar>
2024-11-06 00:27:28 -0300

    saco console

--- src/services/Game/Cards/blockFig.js
+++ src/services/Game/Cards/blockFig.js
@@ -1,8 +1,7 @@
 import { BLOCK_FIG_CARD } from "../../../utils/Constants";
 
-const blockFig = async (playerId, selecFormedFig, figToBlock) => {
-    // console.log(selecFormedFig);
-    console.log(figToBlock);
+const blockFig = async (localPlayerId, selecFormedFig, figToBlock) => {
+
     const fig_card_id = parseInt(figToBlock.id, 10);
     const boxCards = selecFormedFig.map((card) => {
         return { x_pos: card.x, y_pos: card.y };
@@ -15,7 +14,7 @@ const blockFig = async (playerId, selecFormedFig, figToBlock) => {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                player_id: playerId,
+                player_id: localPlayerId,
                 id_fig_card: fig_card_id,
                 figura: boxCards,
             }),
