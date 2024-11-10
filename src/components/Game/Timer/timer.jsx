ef9df72204de127da1b61dbb0f65640b719cfdb5
jazzffernandez <jazmin.fernandez@mi.unc.edu.ar>
2024-11-10 17:53:25 -0300

    timer persistente

--- src/components/Game/Timer/timer.jsx
+++ src/components/Game/Timer/timer.jsx
@@ -3,12 +3,19 @@ import Countdown from "react-countdown";
 import "./timer.css";
 
 const CountdownTimer = ({ resetTimer, onResetCompleted }) => {
-  const [time, setTime] = useState(Date.now() + 120000); // 2 minutos
+  const storedTime = sessionStorage.getItem("countdownTime"); // Busco el tiempo guardado si es que hay
+  const initialTime = storedTime
+    ? parseInt(storedTime, 10) //si hay stored time lo pienso como inicial, sino establezco un nuevo tiempo inicial
+    : Date.now() + 120000; // 2 minutos = 120.000 milisegundps
+
+  const [time, setTime] = useState(initialTime);
 
   // Función para reiniciar el temporizador cuando se llama resetTimer externamente
   useEffect(() => {
     if (resetTimer) {
-      setTime(Date.now() + 120000);
+      const newTime = Date.now() + 120000;
+      setTime(newTime);
+      sessionStorage.setItem("countdownTime", newTime); // Guarda el nuevo tiempo reiniciadp en sessionstorage
       onResetCompleted();
     }
   }, [resetTimer, onResetCompleted]);
@@ -21,6 +28,11 @@ const CountdownTimer = ({ resetTimer, onResetCompleted }) => {
       </span>
     );
   };
+
+  useEffect(() => {
+    // Cada vez que el tiempo cambia,se gguarda en sessionstorage
+    sessionStorage.setItem("countdownTime", time);
+  }, [time]);
 
   return (
     <Countdown key={time} date={time} renderer={renderer} autoStart={true} />
