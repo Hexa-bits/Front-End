9e7cf1a5a715c8641d346569cb9e78054367220a
alvaro-mediina <alvaro.medina@mi.unc.edu.ar>
2024-11-11 15:40:59 -0300

    Lock en partidas privadas

--- src/components/Home/Game_Item/Game_Item.jsx
+++ src/components/Home/Game_Item/Game_Item.jsx
@@ -2,12 +2,26 @@ import React from 'react'
 import Button from '../../Button/Button.jsx'
 import './Game_Item.css'
 
-
-function Game_Item({game, handleJoin}){
+function Game_Item({game, handleJoin, isPrivate}){
+    isPrivate = game.isPrivate
     return(
         <div className="List__Items">
                 <div className="game">
-                    <div className="config__left">{game.game_name}</div>
+                    <div className="config__left">
+
+                        {isPrivate && 
+                            <div className="lock">
+                                <img src='../../../../assets/icons/lock.svg'/>
+                                {game.game_name}
+                            </div>
+                            }
+
+                        {!isPrivate && 
+                            <div className="non-lock">
+                                {game.game_name}
+                            </div>
+                        }
+                    </div>
                     <div className="config__right">
                         <div className="range_players">
                             {game.current_players}/{game.max_players}
