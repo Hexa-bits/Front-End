9ea1eb00909eec5e5220e871e5ebb8ae9b431bd2
alvaro-mediina <alvaro.medina@mi.unc.edu.ar>
2024-11-11 16:47:26 -0300

    Nuevos estilos y arreglo componente para distinción entre partida publica y privada

--- src/components/Home/Game_Item/Game_Item.jsx
+++ src/components/Home/Game_Item/Game_Item.jsx
@@ -2,16 +2,30 @@ import React from 'react'
 import Button from '../../Button/Button.jsx'
 import './Game_Item.css'
 
-function Game_Item({game, handleJoin, isPrivate}){
-    isPrivate = game.isPrivate
+function Game_Item({ game, handleJoin }){
     return(
         <div className="List__Items">
             <div className={`game ${game.started ? "started" : ""}`}>
                 <div className="config__left">
-                    {game.game_name}
-                    {game.started ? 
-                        <img src="/assets/icons/usuario.png" className='started-icon'/>
-                    : ""}
+                    {game.isPrivate && 
+                        <div className="lock">
+                            <img src='../../../../assets/icons/lock.svg'/>
+                            {game.game_name}
+                            {game.started ? 
+                                <img src="/assets/icons/usuario.png" className='started-icon'/>
+                            : ""}
+                        </div>
+                    }
+
+                    {!game.isPrivate && 
+                        <div className="non-lock">
+                            <div className='online-orb'></div>
+                            {game.game_name}
+                            {game.started ? 
+                                <img src="/assets/icons/usuario.png" className='started-icon'/>
+                            : ""}
+                        </div>
+                    }
                 </div>
                 <div className="config__right">
                     <div className="range_players">
